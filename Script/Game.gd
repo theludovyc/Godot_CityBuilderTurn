0e041ff44318e301b20c677eb07f5bf6d9ca6c67
theludovyc <theludovyc@gmail.com>
2020-01-01 14:32:42 +0100

    buildings

--- Script/Game.gd
+++ Script/Game.gd
@@ -12,7 +12,9 @@ var resources = ["Nourriture"]
 
 var buildings = []
 
-var build_adds:PoolByteArray
+var build_adds:Dictionary
+
+var build_ups:Dictionary
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,11 +22,11 @@ func _ready():
 	
 	$Hbox_Hab/Hab.text = str(hab)
 	
-	buildings.push_back(Factory.new("Ferme", 0, 3, 1, 1) )
-
-	build_adds.push_back(0)
+	buildings.push_back(Building.new("Ferme") )
+	buildings.push_back(Building.new("Hutte") )
 	
-	$ItemList.add_item(buildings[0].name)
+	for building in buildings:
+		$ItemList.add_item(building.name)
 	
 	pass # Replace with function body.
 
@@ -38,14 +40,48 @@ func _on_NouvTour_button_down():
 	
 	$Hbox_Nou/NouTot.text = str(nouTot)
 	
-	pass # Replace with function body.
+	$ItemList2.clear()
+	
+	for key in build_adds:
+		if build_ups.has(key):
+			build_ups[key][1] += build_adds[key][1]
+			
+			$ItemList3.set_item_text(build_ups[key][0], key + " x" + str(build_ups[key][1]) )
+		else:
+			build_ups[key]=[$ItemList3.get_item_count(), build_adds[key][1] ]
+			
+			$ItemList3.add_item(key + " x" + str(build_ups[key][1]) )
 
+	build_adds.clear()
+	
+	pass # Replace with function body.
 
 func _on_BtnAdd_button_down():
 	var index = $ItemList.get_selected_items()[0]
+	var key = buildings[index].name
 	
-	build_adds[index] += 1
+	if build_adds.has(key):
+		build_adds[key][1] += 1
+		
+		$ItemList2.set_item_text(build_adds[key][0], key + " x" + str(build_adds[key][1]) )
+	else:
+		build_adds[key]=[$ItemList2.get_item_count(), 1]
+		
+		$ItemList2.add_item(key + " x1")
 	
-	$ItemList2.add_item(buildings[index].name + " x" + str(build_adds[index]) )
+	pass # Replace with function body.
+
+
+func _on_BtnCel_button_down():
+	var index = $ItemList.get_selected_items()[0]
+	var key = buildings[index].name
 	
+	if build_adds[key][1] == 1:
+		$ItemList2.remove_item(build_adds[key][0])
+		
+		build_adds.erase(key)
+	else:
+		build_adds[key][1] -= 1
+		
+		$ItemList2.set_item_text(build_adds[key][0], key + " x" + str(build_adds[key][1]) )
 	pass # Replace with function body.
