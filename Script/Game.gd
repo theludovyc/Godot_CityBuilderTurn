e9d0ba76ebab2e0582d564788392cc6e123de69c
theludovyc <theludovyc@gmail.com>
2020-01-07 23:31:34 +0100

    Hab

--- Script/Game.gd
+++ Script/Game.gd
@@ -6,7 +6,9 @@ extends Node
 
 var nouTot=100
 
-var hab = 5
+var hab = 3
+var habAdd = 0
+var habMax = 3
 
 var resources = ["Nourriture"]
 
@@ -21,9 +23,14 @@ func _ready():
 	$Hbox_Nou/NouTot.text = str(nouTot)
 	
 	$Hbox_Hab/Hab.text = str(hab)
+	$Hbox_Hab/HabMax.text = str(habMax)
+	
+	buildings.push_back(Building.new("Hutte") )
+	
+	build_ups[buildings[0].name]=[0, 1]
+	$ItemList3.add_item(buildings[0].name + " x1")
 	
 	buildings.push_back(Building.new("Ferme") )
-	buildings.push_back(Building.new("Hutte") )
 	
 	for building in buildings:
 		$ItemList.add_item(building.name)
@@ -34,11 +41,32 @@ func _ready():
 #func _process(delta):
 #	pass
 
+func resetHabAdd():
+	if habAdd > 0:
+		$Hbox_Hab/HabAdd.text = "+" + str(habAdd)
+		$Hbox_Hab/HabAdd.set("custom_colors/font_color", Color(0, 1, 0) )
+		return
+	else:
+		$Hbox_Hab/HabAdd.text = str(habAdd)
+		if habAdd == 0:
+			$Hbox_Hab/HabAdd.set("custom_colors/font_color", Color(1, 1, 1) )
+			return
+		else:
+			$Hbox_Hab/HabAdd.set("custom_colors/font_color", Color(1, 0, 0) )
+			return
 
 func _on_NouvTour_button_down():
 	nouTot -= 3*hab
 	
 	$Hbox_Nou/NouTot.text = str(nouTot)
+	
+	hab += habAdd
+	
+	habAdd=0
+	
+	resetHabAdd()
+	
+	$Hbox_Hab/Hab.text = str(hab)
 	
 	$ItemList2.clear()
 	
@@ -84,4 +112,17 @@ func _on_BtnCel_button_down():
 		build_adds[key][1] -= 1
 		
 		$ItemList2.set_item_text(build_adds[key][0], key + " x" + str(build_adds[key][1]) )
+	pass # Replace with function body.
+
+func _on_BtnHabAdd_button_down():
+	if habAdd < (habMax-hab):
+		habAdd += 1
+		resetHabAdd()
+	pass # Replace with function body.
+
+
+func _on_BtnHabRem_button_down():
+	if habAdd > -hab:
+		habAdd -= 1
+		resetHabAdd()
 	pass # Replace with function body.
